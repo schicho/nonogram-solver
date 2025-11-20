6a4bc140c927aae2205b0fdd46993c1837516078
Theo Schneider <schth67@laposte.net>
2025-11-20 15:11:10 +0100

    Revert "Removed Switch in MergeBlockPositions"
    This reverts commit 4ced0592352346b69e7a8928a009700ceba3ac2e.

--- solver.h
+++ solver.h
@@ -4,7 +4,7 @@
 #include "stacks.h"
 #include "types.h"
 
-void MergeBlockPositions(Line*, int);
+Line* MergeBlockPositions(Line*, int, int);
 void ExamineBlocks(Line*, int, int, int, Stack*, int);
 int solveline(Puzzle*, Stack**, Stack*, int);
 void solve(Puzzle*, Stack**, Stack*, int);
